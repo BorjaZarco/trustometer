2fa81cf628169c48253f92467bd2b20365574de6
adrianlorenzodev <defuzah@gmail.com>
2018-10-20 23:40:58 +0100

    Final version

---
+++ SERVER/api/python/newsapi.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+from pprint import pprint
+import urllib.request
+import re
+import requests
+import json
+
+def get_news(keywords):
+    keyword = 'q='+re.sub(r"\s+", ' ', keywords)+'&'
+    url = ('https://newsapi.org/v2/everything?'+keyword+'from=2018-10-01&sortBy=popularity&apiKey=<PLACE_YOUR_NEWSAPIKEY_HERE>')
+    res = (requests.get(url)).json()["articles"]
+    newsTitles = []
+    for news in res:
+        newsTitles.append((news["title"]+".").lower())
+    return newsTitles
